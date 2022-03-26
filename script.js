20048920368304875cfa378e97130024571397a0
Feomatar4eg <feomatar4eg@gmail.com>
2022-03-26 14:26:12 +0300

    выполниел второе задание

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = 'Название проекта';
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 2000;
+const rollback = 20;
+const fullPrice = 5000;
+const adaptive = true;
 
-alert('Hello alert');
-console.log('Hello console');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+console.log(screens.toLocaleLowerCase().split(', '));
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback / 100));
