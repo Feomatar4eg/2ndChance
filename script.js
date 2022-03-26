47b0d6840d9aaa17b1b715c7f3420dcdcf2fda22
Feomatar4eg <feomatar4eg@gmail.com>
2022-03-26 18:57:14 +0300

    lesson05 с сложным 0105

--- script.js
+++ script.js
@@ -1,43 +1,78 @@
 'use strict';
 /*
-1) Объявить функцию getAllServicePrices. Функция возвращает сумму всех дополнительных услуг.
- Результат сохраняем в переменную allServicePrices. Тип - function expression
-2) Объявить функцию getFullPrice. Функция возвращает сумму стоимости верстки
- и стоимости дополнительных услуг (screenPrice + allServicePrices).
-  Результат сохраняем в переменную fullPrice. Тип - function declaration
-3) Объявить функцию getTitle. Функция возвращает title меняя его таким образом:
- первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка
-  может начинаться с пустых символов. " КаЛьКулятор Верстки"
-4) Объявить функцию getServicePercentPrices. Функция возвращает итоговую стоимость
- за вычетом процента отката. Результат сохраняем в переменную servicePercentPrice
-  (итоговая стоимость минус сумма отката)
-5) Вывести в консоль строку из переменной screens в виде массива
-6) Почистить консоль логи и добавить недостающие, должны остаться:
-- вызовы функции showTypeOf
-- вывод строки с типами экранов для разработки screens
-- сообщение о скидке пользователю (вызовы функции getRollbackMessage)
-- стоимость за вычетом процента отката посреднику (вызовы функции getServicePercentPrices)
-6) Проверить, чтобы все работало и не было ошибок в консоли
-7) Добавить папку с четвертым уроком в свой репозиторий на GitHub
- */
+ОБЯЗАТЕЛЬНОЕ ЗАДАНИЕ:
+1) Переписать получение значения переменной screenPrice циклом do while.
+ Вопрос должен задаваться один раз обязательно, далее уже по условию
+2) Добавить проверку что введённые данные являются числом, которые мы
+ получаем на вопрос "Сколько это будет стоить" в функции getAllServicePrices
+3) Поправить проект так, чтоб расчеты велись верно. Проверить типы получаемых
+ переменных и привести их к нужным.
+4) Проверить, чтобы все работало и не было ошибок в консоли.
+5) Добавить папку с уроком в свой репозиторий на GitHub
+УСЛОЖНЕННОЕ ЗАДАНИЕ №1:
+Придумать способ сохранять в переменную ответ пользователя после проверки
+на число именно как число при любом вводе. (с пробелами и без в переменную 
+заносилось именно число) На данный момент проверка isNumber пропустит такой вариант "
+123   " и именно это значение попадет в переменную. Необходимо это исправить. Так же
+ учитывайте что человек может нажать отмену и в проверку уйдет значение NULL
+*/
 
-let title = prompt('Введите название проекта.', '  кальКУЛЯТОр верстки.');
-const screens = prompt("“Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-const screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-const adaptive = confirm("Нужен ли адаптив на сайте?");
-const service1 = prompt("Какой дополнительный тип услуги нужен?", "Яндекс метрика");
-const servicePrice1 = +prompt("Сколько это будет стоить?", '2000');
-const service2 = prompt("Какой дополнительный тип услуги нужен?", "Гугл метрика");
-const servicePrice2 = +prompt("Сколько это будет стоить?", '2000');
-
-const rollback = 20;
-
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
 let servicePercentPrice;
-let allServicePrices, fullPrice;
+let service1;
+let service2;
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const asking = function () {
+
+    title = prompt('Введите название проекта.', '  кальКУЛЯТОр верстки.');
+    screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+    screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+    while (!isNumber(screenPrice)) {
+        screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+    }
+    screenPrice = Number(screenPrice);
+    adaptive = confirm("Нужен ли адаптив на сайте?");
+
+};
+
+const getAllServicePrices = function () {
+    let sum = 0;
+    let i = 0;
+    let servicePrice;
+    do {
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги нужен?", "Яндекс метрика");
+        } else if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги нужен?", "Гугл метрика");
+        }
+        servicePrice = prompt("Сколько это будет стоить?", '2000');
+        while (!isNumber(servicePrice)) {
+            servicePrice = prompt("Сколько это будет стоить?", '2000');
+        }
+        sum += Number(servicePrice);
+        i++;
+    }
+    while (i < 2);
+    return sum;
+};
 
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
 };
+
+function getFullPrice() {
+    return screenPrice + allServicePrices;
+}
 
 const getRollbackMsg = function (price) {
     if (price >= 30000) {
@@ -51,32 +86,27 @@ const getRollbackMsg = function (price) {
     }
 };
 
-const getAllServicePrices = function (price1, price2) {
-    return price1 + price2;
+
+const getTitle = function () {
+    title = title.trim().toLowerCase();
+    title = title[0].toUpperCase() + title.slice(1);
+    return title;
 };
 
-function getFullPrice(scrPrice, servicesPrice) {
-    return scrPrice + servicesPrice;
-}
-
-const getTitle = function (titleText) {
-    titleText = titleText.trim().toLowerCase();
-    titleText = titleText[0].toUpperCase() + titleText.slice(1);
-    return titleText;
-};
-
-const getServicePercentPrices = function (fullPrice, rollback) {
+const getServicePercentPrices = function () {
     return Math.round(fullPrice - (fullPrice * (rollback / 100)));
 };
 
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-title = getTitle(title);
+asking();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
+
 console.log(getRollbackMsg(fullPrice));
 console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
 console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
